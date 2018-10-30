cf42b6e8434f0080712922a841c4511fe3521745
Brad Lund <lundaxbit@gmail.com>
2018-10-30 10:53:39 -0600

    simple script to show variables should prede function

---
+++ vprecedescript.js
@@ -0,0 +1,10 @@
+function findBiggestFraction() {
+  console.log("The function is running!");
+  a>b ? console.log("a: ",a) : console.log("b: ",b);
+}
+
+// variables must precede function! //
+var a = 3/4;
+var b = 5/7;
+
+findBiggestFraction();
